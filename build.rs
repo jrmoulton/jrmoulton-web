e35b955a85f9eebd6a905239a45aba20ef788f91
Jared Moulton <jaredmoulton3@gmail.com>
2022-12-04 14:52:05 -0700

    my wip

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    std::fs::create_dir_all("build/styles").unwrap();
-    std::process::Command::new("sass")
-        .arg("styles/common.scss")
-        .arg("build/styles/common.css")
-        .spawn()
-        .unwrap();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use pulldown_cmark::{CodeBlockKind, Event};
 
 fn main() {
+    std::fs::create_dir_all("build/styles").unwrap();
+    std::process::Command::new("sass")
+        .arg("styles/common.scss")
+        .arg("build/styles/common.css")
+        .spawn()
+        .unwrap();
     for file in std::fs::read_dir("./content/").unwrap() {
         let file = file.unwrap();
         let input = std::fs::read_to_string(file.path()).unwrap();
