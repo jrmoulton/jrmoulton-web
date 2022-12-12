806d4381e63ee517d19a8a2280301b0974bb8b70
Jared Moulton <jaredmoulton3@gmail.com>
2022-12-12 13:51:23 -0700

    Add support for the index.html

--- crates/gen/src/main.rs
+++ crates/gen/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use handlebars::Handlebars;
 use pulldown_cmark::{CodeBlockKind, Event};
 use rayon::prelude::*;
@@ -33,6 +35,18 @@ fn main() {
             include_str!("../../../templates/theme_div.html"),
         )
         .unwrap();
+    templ_reg
+        .register_template_string("article", include_str!("../../../templates/article.html"))
+        .unwrap();
+    templ_reg
+        .register_template_string(
+            "article_preview",
+            include_str!("../../../templates/article_preview.html"),
+        )
+        .unwrap();
+    templ_reg
+        .register_template_string("index", include_str!("../../../templates/index.html"))
+        .unwrap();
 
     // Get all the themes
     let mut themes = Themes::default();
@@ -64,19 +78,42 @@ fn main() {
     // sort the themes so that they are in alphabetical order when the site is generated
     themes.sort_themes();
 
-    write_articles(&mut templ_reg, &mut themes);
+    let latest_articles = write_articles(&mut templ_reg, &mut themes);
+    // things I need for an index.html
+    //
+    // A full article
+    let mut index_article = Article {
+        head: include_str!("../../../templates/head.html"),
+        date: dateparser::parse("12/12/2022").unwrap(),
+        file_name_cap: "Jared Moutlon".to_string(),
+        file_name: "index".to_string(),
+        header: include_str!("../../../templates/header.html"),
+        theme_divs: themes.theme_divs(&templ_reg),
+        page_section: String::new(),
+    };
+    for article in latest_articles.articles {
+        index_article.page_section.push_str(
+            &templ_reg
+                .render("article_preview", &ArticlePreview::from(article))
+                .unwrap(),
+        );
+    }
+    std::fs::write(
+        "build/index.html",
+        templ_reg.render("article", &index_article).unwrap(),
+    )
+    .unwrap();
 }
 
-fn write_articles(templ_reg: &mut Handlebars, themes: &mut Themes) {
-    templ_reg
-        .register_template_string("article", include_str!("../../../templates/article.html"))
-        .unwrap();
+fn write_articles(templ_reg: &mut Handlebars, themes: &mut Themes) -> LatestArticles {
     let files: Vec<_> = std::fs::read_dir("./content/")
         .expect("couldnt read the content directory")
         .collect();
     let theme =
         tree_painter::Theme::from_helix(include_str!("../../../themes/onedark_dark.toml")).unwrap();
+    let latest_articles = Arc::new(Mutex::new(LatestArticles::new()));
     files.par_iter().for_each(|file| {
+        let mut date = dateparser::parse("1/1/2000").unwrap();
         let article_string = file.as_ref().unwrap();
         let input = std::fs::read_to_string(article_string.path()).unwrap();
         let mut renderer = tree_painter::Renderer::new(theme.clone());
@@ -88,16 +125,16 @@ fn write_articles(templ_reg: &mut Handlebars, themes: &mut Themes) {
                 next_lang = lang.to_string();
                 Event::SoftBreak
             }
-            Event::Text(code) if next_lang == "date" => Event::Html(
-                format!(
-                    r#"<div class="date">{}</div>"#,
-                    dateparser::parse(code.trim())
-                        .unwrap()
-                        .date_naive()
-                        .format("%A,  %B %-d, %C%y")
+            Event::Text(code) if next_lang == "date" => {
+                date = dateparser::parse(code.trim()).unwrap();
+                Event::Html(
+                    format!(
+                        r#"<div class="date">{}</div>"#,
+                        date.date_naive().format("%A,  %B %-d, %C%y")
+                    )
+                    .into(),
                 )
-                .into(),
-            ),
+            }
             Event::Text(code) if !next_lang.is_empty() => {
                 let lang = tree_painter::Lang::from_name(&next_lang).unwrap();
                 let mut code_str = String::new();
@@ -130,13 +167,18 @@ fn write_articles(templ_reg: &mut Handlebars, themes: &mut Themes) {
         let file_name_cap = titlecase::titlecase(&file_name.replace('_', " "));
         let article = Article {
             head: include_str!("../../../templates/head.html"),
+            date,
             file_name_cap,
+            file_name: file_name.to_string(),
             header: include_str!("../../../templates/header.html"),
             theme_divs: themes.theme_divs(templ_reg),
             page_section: mark_out,
         };
         let final_output = templ_reg.render("article", &article).unwrap();
+        latest_articles.lock().unwrap().add_if_latest(article);
 
         std::fs::write(format!("build/{}.html", file_name), final_output).unwrap();
     });
+    let lock = Arc::try_unwrap(latest_articles).expect("Lock still has multiple owners");
+    lock.into_inner().expect("Mutex cannot be locked")
 }
--- crates/gen/src/structs.rs
+++ crates/gen/src/structs.rs
@@ -1,13 +1,45 @@
+use chrono::{DateTime, Utc};
 use handlebars::Handlebars;
+use serde::ser::SerializeStruct;
 use serde::Serialize;
 
-#[derive(Default, Serialize)]
+#[derive(Default, Clone, Debug)]
 pub struct Article {
     pub head: &'static str,
+    pub date: DateTime<Utc>,
     pub file_name_cap: String,
+    pub file_name: String,
     pub header: &'static str,
     pub theme_divs: String,
     pub page_section: String,
+}
+impl Article {
+    pub(crate) fn new() -> Self {
+        Self::default()
+    }
+}
+impl Serialize for Article {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("Article", 6)?;
+        state.serialize_field("head", &self.head)?;
+        state.serialize_field(
+            "date",
+            &self
+                .date
+                .date_naive()
+                .format("%A,  %B %-d, %C%y")
+                .to_string(),
+        )?;
+        state.serialize_field("file_name_cap", &self.file_name_cap)?;
+        state.serialize_field("file_name", &self.file_name)?;
+        state.serialize_field("header", &self.header)?;
+        state.serialize_field("theme_divs", &self.theme_divs)?;
+        state.serialize_field("page_section", &self.page_section)?;
+        state.end()
+    }
 }
 
 #[derive(Default)]
@@ -38,5 +70,60 @@ impl Theme {
             &std::fs::read_to_string(format!("./themes/{}.toml", &self.base_name)).unwrap(),
         )
         .unwrap()
+    }
+}
+
+#[derive(Default, Debug, Serialize)]
+pub struct ArticlePreview {
+    theme_divs: String,
+    article_link: String,
+    article_title: String,
+    short_content: String,
+}
+
+impl From<Article> for ArticlePreview {
+    fn from(article: Article) -> Self {
+        let article_link = format!("build/{}.html", article.file_name);
+        let re = regex::Regex::new(r#"<\s*p[^>]*>([^<]*)<\s*/\s*p\s*>"#).unwrap();
+        let short_content: String = re
+            .captures_iter(&article.page_section)
+            .take(2)
+            .map(|p| format!(r"{}", p.get(0).unwrap().as_str()))
+            .collect();
+        Self {
+            theme_divs: article.theme_divs,
+            article_link,
+            article_title: article.file_name_cap,
+            short_content,
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct LatestArticles {
+    pub articles: Vec<Article>,
+}
+impl LatestArticles {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add_if_latest(&mut self, article: Article) {
+        let mut pop = false;
+        match self.articles.first() {
+            Some(last) => {
+                if article.date > last.date {
+                    if self.articles.len() > 9 {
+                        pop = true;
+                    }
+                    self.articles.push(article);
+                }
+            }
+            None => self.articles.push(article),
+        }
+        self.articles.sort_by(|a, b| b.date.cmp(&a.date));
+        if pop {
+            self.articles.pop();
+        }
     }
 }
